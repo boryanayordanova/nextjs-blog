463e18718808fc98ecce3148f06e5b20aa1e2294
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2024-06-05 12:21:48 +0300

    priority

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,20 +1,23 @@
-import Image from 'next/image';
-import classes from './hero.module.css';
-export default function Hero(){
-    return (
-        <section className={classes.hero}>
-            <div className={classes.image}>
-                <Image 
-                    src="/images/site/max.png" 
-                    alt="An image" 
-                    width={300} 
-                    height={300}
-                />
-            </div>
-            <h1>Hi, I'm Max</h1>
-            <p> I blog about web development - especially frontend frameworks like
-        Angular or React.</p>
-
-        </section>
-    )
+import Image from "next/image";
+import classes from "./hero.module.css";
+export default function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+      <h1>Hi, I'm Max</h1>
+      <p>
+        {" "}
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
 }
